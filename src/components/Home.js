c1b074da96231efd3971875c1002af3d43e928c2
Sorin Chis <kish.sorin@yahoo.com>
2020-02-06 14:25:12 +0200

    added form for getting data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { observer } from "mobx-react";
 
+const initialFormValues = {
+  name: "",
+  quantity: "",
+  price: ""
+};
+
 const Home = ({ invoice }) => {
+  const [formData, setFormData] = useState(initialFormValues);
+  const nameRef = useRef(null);
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(formData);
+    const fromatData = {
+      name: formData.name,
+      quantity: parseInt(formData.quantity, 10),
+      price: parseFloat(formData.price)
+    };
+    invoice.itemList.add(fromatData);
+    setFormData(initialFormValues);
+    nameRef.current.focus();
+  };
+
+  const handleChange = ({ target: { name, value }, reset }) => {
+    console.log(name, value);
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
   return (
     <div>
       <h1>{invoice.status()}</h1>
       {!invoice.is_paid && <button onClick={invoice.markPaid}>Pay</button>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            onChange={handleChange}
+            name="name"
+            ref={nameRef}
+            value={formData.name}
+          />
+        </label>
+        <label htmlFor="quantity">
+          Quantity
+          <input
+            type="number"
+            onChange={handleChange}
+            name="quantity"
+            value={formData.quantity}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="float"
+            onChange={handleChange}
+            name="price"
+            value={formData.price}
+          />
+        </label>
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
--- src/models/ItemList.js
+++ src/models/ItemList.js
@@ -1,8 +1,15 @@
 import { types } from "mobx-state-tree";
 import Item from "./Item";
 
-const ItemList = types.model("ItemList", {
-  items: types.array(Item)
-});
+const ItemList = types
+  .model("ItemList", {
+    items: types.array(Item)
+  })
+  .actions(self => ({
+    add(item) {
+      console.log("INSIDE MST", item);
+      self.items.push(item);
+    }
+  }));
 
 export default ItemList;
